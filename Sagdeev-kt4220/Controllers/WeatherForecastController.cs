9f737e454f7b3d1e2978d6c7eaa3cbbbd5a3275f
Aska Lakura <askalakura@gmail.com>
2023-09-18 14:09:04 +0300

    add logging and WeatherForecast test request

--- Sagdeev-kt4220/Controllers/WeatherForecastController.cs
+++ Sagdeev-kt4220/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Sagdeev_kt4220.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "пора за грибами"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -21,6 +21,8 @@ namespace Sagdeev_kt4220.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogError("Method was called");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -28,6 +30,15 @@ namespace Sagdeev_kt4220.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+        [HttpPost(Name = "AddNewSummary")]
+        public string[] AddNewSummary(string newSummary)
+        {
+            _logger.LogError("New method was called");
+
+            var list = Summaries.ToList();
+            list.Add(newSummary);
+            return list.ToArray();
         }
     }
 }
